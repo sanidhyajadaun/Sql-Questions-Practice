ddeb49c3f16e459a1212dacf59fda3569b867283
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2023-08-06 10:42:09 +0530

    Consecutive Numbers

---
+++ Problems/26 - Consecutive Numbers.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT DISTINCT
+    i1.num AS ConsecutiveNums
+FROM 
+    logs i1,
+    logs i2,
+    logs i3
+WHERE 
+    i1.id = i2.id+1 AND
+    i2.id = i3.id+1 AND
+    i1.num = i2.num AND
+    i2.num = i3.num;
